df30ec886d74374af0a92e249aec5b58eed05668
kvadratikk <adahiter@gmail.com>
2021-08-11 05:11:00 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+    if (!matrix || matrix.length === 0) return []
+
+    let res = []
+
+    for (let i = 0; i < matrix.length; i++) {
+        if (i % 2 === 0) {
+            iterationEven(matrix[i], res)
+        } else {
+            iterationOdd(matrix[i], res)
+        }
+    }
+
+    return res
+}
+
+function iterationEven(arr, res) {
+    for (let i = 0; i < arr.length; i++) {
+        res.push(arr[i])
+    }
+}
+
+function iterationOdd(arr, res) {
+    for (let i = arr.length - 1; i >= 0; i--) {
+        res.push(arr[i])
+    }
 }
